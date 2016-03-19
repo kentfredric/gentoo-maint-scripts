c14318dbe59813e3c451c0f156eb27901cf3699d
Kent Fredric <kentfredric@gmail.com>
2016-03-19 16:05:53 +1300

    cleanup old scripts

--- rebase.sh
+++
@@ -1,3 +0,0 @@
-for i in $(cat pending.txt); do
-  git checkout $i && git rebase upstream/master && git push -f kentnl $i
-done
--- rebase_push.sh
+++
@@ -1,1 +0,0 @@
-git checkout $1 && git rebase upstream/master && git push -f kentnl $1
