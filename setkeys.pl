a2bbcc833b64c623b2a5ff933cac54c9568e4a5a
Kent Fredric <kentfredric@gmail.com>
2016-07-25 17:35:44 +1200

    Add setkeys util

--- setkeys.pl
+++ setkeys.pl
@@ -8,13 +8,30 @@ use Path::Tiny qw( path cwd );
 use File::pushd qw( pushd );
 use Capture::Tiny qw( capture_stdout );
 
-my $KEYWORD_STRING="~alpha";
+#my $KEYWORD_STRING=$ENV{WANT_KEYWORDS};
+#"alpha amd64 ppc ~ppc64 ~sparc x86 ~amd64-linux ~x86-linux ~ppc-macos ~x86-macos ~sparc-solaris";
 
+my (%KEYWORDS) = map { $_ =~ s/^~//; $_ => 1 } split " ", $ENV{WANT_KEYWORDS}; #$KEYWORD_STRING;
 
-my (%KEYWORDS) = map { $_ =~ s/^~//; $_ => 1 } split " ", $KEYWORD_STRING;
+my (@STABLE) = qw( alpha amd64 arm hppa ia64 pcc pcc64 sparc x86 );
+my (@DEV)    = qw( alpha amd64 amd64-fbsd arm arm64 hppa ia64 m68k mips ppc ppc64 sh sparc x86 x86-fbsd );
+my (@EXP)    = qw( amd64 amd64-fbsd arm arm64 mips nios2 ppc ppc64 riscv s390 sh sparc-fbsd );
+my (@PREFIX) = qw(
+  amd64-linux
+  arm-linux
+  hppa-hpux
+  ia64-hpux
+  m68k-mint
+  ppc64-linux
+  ppc-aix ppc-macos ppc-openbsd
+  sparc64-freebsd sparc64-solaris
+  sparc-solaris
+  x64-cygwin x64-freebsd x64-macos x64-openbsd x64-solaris
+  x86-cygwin x86-freebsd x86-interix x86-linux x86-macos x86-netbsd x86-openbsd x86-solaris
+  x86-winnt
+);
 
-my (%DEV_KEYWORDS) = map { $_ => 1 } qw( alpha amd64 arm arm64 hppa ia64 m68k mips nios2 ppc ppc64
-                                         riscv s390 sh sparc x86 );
+my (%DEV_KEYWORDS) = map { $_ => 1 } @STABLE, @DEV, @EXP;
 
 #my %KEYWORDS = map { $_ => 1 } qw( arm64 m68k mips s390 sh );
 
