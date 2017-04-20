f50afa683c0a01c0f882f141daf2515f994fd5a3
Kent Fredric <kentfredric@gmail.com>
2017-04-21 01:49:12 +1200

    modernize packlisttometaxml.pl

--- pm_new.pl
+++ pm_new.pl
@@ -11,20 +11,26 @@ use Path::Tiny qw( path cwd );
 use Capture::Tiny qw( capture_stdout );
 use Gentoo::PerlMod::Version qw( gentooize_version );
 
-my $catpkg  = cwd_cat_pkg( repo, cwd() );
-my $version = args_version( \@ARGV );
-my $gv      = gentooize_version( $version, { lax => 1 } );
+my $catpkg   = cwd_cat_pkg( repo, cwd() );
+my $version  = args_version( \@ARGV );
+my $gv       = gentooize_version( $version, { lax => 1 } );
 my $basename = "$catpkg->{package}-$gv.ebuild";
-my $basedir  = path(repo, $catpkg->{category}, $catpkg->{package} );
+my $basedir  = path( repo, $catpkg->{category}, $catpkg->{package} );
 my $destfile = $basedir->child($basename);
 
-my $template = get_newest_ebuild( repo, $catpkg->{category}, $catpkg->{package} );
-$template = fix_year( $template, 2016 );
+my $template =
+  get_newest_ebuild( repo, $catpkg->{category}, $catpkg->{package} );
+$template = fix_year( $template, 1900 + [localtime]->[5] );
 $template = force_eapi6($template);
 $template = set_version( $template, $version );
+if ( $ENV{DIST_AUTHOR} ) {
+    $template = set_author( $template, $ENV{DIST_AUTHOR} );
+}
 $destfile->spew_raw($template);
 ensure_xml( repo, $catpkg->{category}, $catpkg->{package} );
 drop_keywords($destfile);
+system( 'git', 'add', '.' );
+system( 'repoman', 'manifest' );
 
 sub cwd_cat_pkg {
     my ( $repo, $cwd ) = @_;
@@ -62,12 +68,12 @@ sub get_newest_ebuild {
 
 sub ensure_xml {
     my ( $repo, $cat, $pkg ) = @_;
-    my $workd = path($repo)->child($cat,$pkg);
+    my $workd = path($repo)->child( $cat, $pkg );
     my $xml = $workd->child('metadata.xml');
     if ( not -e $xml ) {
-      my $modname = $pkg;
-      $modname =~ s/-/::/g;
-      $xml->spew_raw(generate_xml_template($pkg, $modname));
+        my $modname = $pkg;
+        $modname =~ s/-/::/g;
+        $xml->spew_raw( generate_xml_template( $pkg, $modname ) );
     }
 }
 
@@ -109,47 +115,55 @@ sub set_version {
     $content =~ s{(^inherit perl-module)}{DIST_VERSION=$version\n$1}msx;
     return $content;
 }
+
+sub set_author {
+    my ( $content, $author ) = @_;
+    if ( $content =~ /DIST_AUTHOR/ ) {
+        $content =~ s/^DIST_AUTHOR=(.*?)$/DIST_AUTHOR=$author/msx;
+        return $content;
+    }
+    $content =~ s{(^inherit perl-module)}{DIST_AUTHOR=$author\n$1}msx;
+    return $content;
+}
+
 sub drop_keywords {
-    my ( $ebuild ) = @_;
+    my ($ebuild) = @_;
     my $cwd = cwd();
     chdir $ebuild->parent;
-    eval {
-      system("ekeyword", "~all", $ebuild->basename );
-    };
+    eval { system( "ekeyword", "~all", $ebuild->basename ); };
     chdir $cwd;
 }
 
 sub generate_ebuild_template {
     my ( $cat, $package ) = @_;
     my $content = <<"EOF";
-# Copyright 1999-2016 Gentoo Foundation
+# Copyright 1999-2017 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# \$Id\$
 
 EAPI=6
 
-DIST_AUTHOR=???
-DIST_VERSION=???
+DIST_AUTHOR=\$(die missing author)
+DIST_VERSION=\$(die missing version)
 inherit perl-module
 
-DESCRIPTION=???
+DESCRIPTION=\$(die missing description)
 SLOT="0"
 KEYWORDS="~amd64 ~x86"
 IUSE="test"
 
 RDEPEND=""
 DEPEND="\${RDEPEND}
-  test? (
-    ...
-  )
+	test? (
+		
+	)
 "
 EOF
 
 }
 
 sub generate_xml_template {
-  my ( $distname, $modulename ) = @_;
-my $content = <<"EOF";
+    my ( $distname, $modulename ) = @_;
+    my $content = <<"EOF";
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE pkgmetadata SYSTEM "http://www.gentoo.org/dtd/metadata.dtd">
 <pkgmetadata>
